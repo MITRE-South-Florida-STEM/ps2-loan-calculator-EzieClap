3d2f96b1c1dca6348c275dd1f233bbe9b16e32e8
Frank Gonzalez <102252315+EzieClap@users.noreply.github.com>
2022-04-11 01:54:30 +0000

    In progress

--- ps1a.py
+++ ps1a.py
@@ -1,1 +1,16 @@
+'Data'
+annual_salary = float(input('Enter your annual salary: '))
+portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
+total_cost = float(input('Enter the cost of your dream home: '))
+portion_down_payment = 0.25
+current_savings = 0
+r = 0.04
+monthly_r=r/12
+monthly_salary = annual_salary/12
+m = 0
+
+'Calculations'
+while current_savings < total_cost*portion_down_payment:
+  current_savings += current_savings * monthly_r + (monthly_salary*portion_saved)
+print('Number of months: ' + str(m))
 
--- ps1c.py
+++ ps1c.py
@@ -1,1 +1,4 @@
 
+
+print("Best saving rate: ", guess)
+print("With current savings: ", current_savings)
